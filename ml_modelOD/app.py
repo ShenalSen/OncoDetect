bf0eaf5b248ba31bb7a9e508852e431dc9ca6050
ShenalSen <shenal.unc555@gmail.com>
2024-12-20 13:06:51 +0530

    Add model loading functionality with error handling in Streamlit app

--- ml_modelOD/app.py
+++ ml_modelOD/app.py
@@ -3,3 +3,13 @@ import streamlit as st
 from tensorflow.keras.models import load_model  # type: ignore
 import cv2
 import numpy as np
+
+
+# Load the trained model
+try:
+    SAVED_MODEL_PATH = r"C:\\F\\ml_modelOD\\saved_model\\best_model.h5"
+    model = load_model(SAVED_MODEL_PATH)
+    st.success("Model loaded successfully!")
+except Exception as e:
+    st.error(f"Error loading model: {e}")
+    st.stop()
