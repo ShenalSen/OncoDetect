068d6a4ce94567c2195a704dc474b375092d5a2e
Sanjula <163961280+SNBlazei@users.noreply.github.com>
2025-02-07 19:43:59 +0530

    Delete OncoDetectBackend directory

--- OncoDetectBackend/patients/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("list/", views.list_patients, name="list_patients"),
-    path("create/", views.create_patient, name="create_patient"),
-]
