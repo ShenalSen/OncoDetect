76d6fc57c9378a25863eefbcd5d390bfaa830d73
Sanjula <163961280+SNBlazei@users.noreply.github.com>
2025-02-08 19:56:26 +0530

    patients api

--- OncoDetectBackend/OncoDetectBackend/patients/models.py
+++ OncoDetectBackend/OncoDetectBackend/patients/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    patient_id=models.CharField(max_length=100,unique=True)
+    name=models.CharField(max_length=200)
+    age=models.IntegerField()
+    contact_number=models.CharField(max_length=15)
+    appointment_id=models.CharField(max_length=100)
+    uploaded_file=models.FileField(upload_to="uploads/",blank=True,null=True)
+
+
+
+    
+    def __str__(self):
+        return self.name
+    
