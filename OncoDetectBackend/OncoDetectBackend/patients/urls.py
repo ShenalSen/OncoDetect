286bd5411e1c5cddcb7f8122eac541eca5df3ef8
Sanjula <163961280+SNBlazei@users.noreply.github.com>
2025-02-15 08:59:23 +0530

    delete patients

--- OncoDetectBackend/OncoDetectBackend/patients/urls.py
+++ OncoDetectBackend/OncoDetectBackend/patients/urls.py
@@ -4,4 +4,5 @@ from .views import PatientListCreateView, PatientDetailView
 urlpatterns = [
     path("patients/", PatientListCreateView.as_view(), name="patient-list"),
     path("patients/<int:pk>/", PatientDetailView.as_view(), name="patient-detail"),
+    
 ]
