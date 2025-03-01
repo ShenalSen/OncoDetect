e3d243d82f6cbc2e21fa5bb3a52992e4ecb0c931
Sanjula <163961280+SNBlazei@users.noreply.github.com>
2025-03-01 21:21:26 +0530

    Added route for get all appointments

--- Flask backend/main.py
+++ Flask backend/main.py
@@ -259,5 +259,16 @@ def create_appointment():
 
     return jsonify({'message': 'Appointment created successfully'}), 201
 
+# Get all appointments
+@app.route('/appointments', methods=['GET'])
+def get_appointments():
+    appointments = Appointment.query.all()
+    return jsonify([{
+        'patient_id': appointment.patient_id,
+        'doctor_id': appointment.doctor_id,
+        'appointment_date': appointment.appointment_date,
+        'description': appointment.description
+    } for appointment in appointments]), 200
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
